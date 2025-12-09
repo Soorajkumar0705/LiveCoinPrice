a402e23d6bf812e20478e614fa2593644663b66d
suraj kahar <surajkahar0705@gmail.com>
2025-12-09 20:54:54 +0530

    Initial Commit

---
+++ LiveCoinPrice/LiveCoinPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCoinPriceApp.swift
+//  LiveCoinPrice
+//
+//  Created by sooraj kahar on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveCoinPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
